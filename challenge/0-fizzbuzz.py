7a00648ba3a6ec856551475f164900c2c4e15ec3
xhoi700 <10057@holbertonstudents.com>
2025-05-21 09:44:58 +0000

    added

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,23 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
-def fizzbuzz(n):
-    for i in range(1, n + 1):
-        if i % 15 == 0:
-            print("FizzBuzz", end=' ')
-        elif i % 3 == 0:
-            print("Fizz", end=' ')
-        elif i % 5 == 0:
-            print("Buzz", end=' ')
-        else:
-            print(i, end=' ')
+if len(sys.argv) != 2:
+    print("Usage: ./0-fizzbuzz.py <number>")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-fizzbuzz.py <number>")
+try:
+    limit = int(sys.argv[1])
+except ValueError:
+    print("Please enter a valid integer.")
+    sys.exit(1)
+
+for i in range(1, limit + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
     else:
-        try:
-            n = int(sys.argv[1])
-            fizzbuzz(n)
-        except ValueError:
-            print("Please enter a valid integer.")
+        print(i, end=" ")
+print()
